74fdd5bff7864c23ab9b33d56596c47bbde3f253
Romina Trazzi <romina.trazzi@posta.it>
2020-11-18 03:06:47 +0100

    Commit es. dadi quasi completo
    Il programma calcola i numeri e li confronta correttamente mostrando gli alert adatti, ma non li scrive nelle caselle input.

--- Dadi/assets/js/main.js
+++ Dadi/assets/js/main.js
@@ -6,45 +6,45 @@
 var btnGenera = document.getElementById("btn_genera");
 var btnReset = document.getElementById("btn_reset");
 
-// Creiamo variabili JS per collegarle agli input HTML 
-var num1 = document.getElementById("numero_utente");
-var num2 = document.getElementById("numero_computer");
-
-//Creiamo altre due variabili per selezionare i value degli input
-var numberUser = num1.value;
-var numberPc = num2.value;
-
-// console.log(numberUser, numberPc);
-
 /* Creiamo una funzione di ascolto per l'evento click su btnGenera
 che ora è anche una variabile JS */
 btnGenera.addEventListener("click", function () {
 
-   /* Assegniamo alle variabili dei numeri una formula per calcolare 
+   /* Assegniamo a delle variabili JS una formula per calcolare 
    due numeri random compresi tra 1 e 6 */
-   numberUser = Number(Math.floor(Math.random() * 6) + 1);
-   numberPc = Number(Math.floor(Math.random() * 6) + 1);
+   var numberUser = Number(Math.floor(Math.random() * 6) + 1);
+   var numberPc = Number(Math.floor(Math.random() * 6) + 1);
 
+   //console.log(numberUser, numberPc);
+   
+   // Creiamo variabili JS per collegarle agli input HTML
+   var num1 = document.getElementById("numero_utente");
+   var num2 = document.getElementById("numero_computer");
 
-    // Inseriamo i valori delle variabili JS nell'HTML
-    document.getElementById("numero_utente").innerHTML = numberUser;
-    document.getElementById("numero_computer").innerHTML = numberPc;
-
+   //console.log(num1, num2);
+   
+   // Assegniamo a delle variabili JS i valori degli input HTML
+   var num1Input = num1.value;
+   var num2Input = num2.value;
+   
+   // Inseriamo i risultati delle variabili JS di calcolo al posto dei valori degli input HTML
+   num1Input.innerHTML = numberUser;
+   num2Input.innerHTML = numberPc;
+   
+   // Creiamo messaggi di alert per mostrare i risultati all'utente
+   if (numberUser > numberPc) {
+       alert("Congratulazioni! Hai vinto!");
+    } else if (numberUser < numberPc) {
+        alert("Purtroppo hai perso. Ritenta ancora!");
+    } else {
+        alert("Pareggio!");
+    } 
 });
-
-// Creiamo messaggi di alert per mostrare i risultati all'utente
-if (numberUser > numberPc) {
-    alert("Congratulazioni! Hai vinto!");
-} else if (numberUser == numberPc) {
-    alert("Pareggio!");
-} else {
-    alert("Purtroppo hai perso. Ritenta ancora!");
-}
 
 
 /* Creiamo una funzione di ascolto per l'evento click su btnAnnulla
 che ora è anche una variabile JS per svuotare le variabili */
     btnReset.addEventListener("click", function () {
-        document.getElementById("numero_utente").value = "";
-        document.getElementById("numero_computer").value = "";
+        num1.value = "";  // alias: document.getElementById("numero_utente").value = "";
+        num2.value = "";  // alias: document.getElementById("numero_computer").value = "";
     });
