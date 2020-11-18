34088fb222bac0f38a70d32aa26549fd70266002
Romina Trazzi <romina.trazzi@posta.it>
2020-11-18 06:42:48 +0100

    Commit bozza mail 2
    Aggiunto bottone ricomincia

--- Mail/assets/js/main.js
+++ Mail/assets/js/main.js
@@ -18,9 +18,34 @@ var lista_email = ["alfa@gmail.com", "beta@gmail.com", "gamma@gmail.com", "delta
 
 //console.log(lista_email);
 
+/* Creiamo una variabile per verificare se la mail inserita fa parte di quelle dell'array.
+La risposta può essere true o false */
+var boolean = lista_email.includes(userMail);
+
+console.log(boolean);
+
+
 // Creiamo i messaggi per l'utente
-if (lista_email.includes("userMail")) {
+if (boolean = true) {
     alert("Il controllo è andato a buon fine, sei nella lista!");
 } else {
     alert("Il controllo dice che non sei nella lista!");
 } 
+
+// Raccogliamo in una variabile JS l'id del button 
+var btnAgain = document.getElementById("btn_again");
+
+/* Creiamo una funzione di ascolto per l'evento click su btnAgain
+che ora è anche una variabile JS per far riavviare il prompt */
+btnAgain.addEventListener("click", function () {
+    userMail = prompt("Inserisci la tua email");
+    lista_email = ["alfa@gmail.com", "beta@gmail.com", "gamma@gmail.com", "delta@gmail.com", "eta@gmail.com"];
+    boolean = lista_email.includes(userMail);
+
+    if (boolean = true) {
+        alert("Il controllo è andato a buon fine, sei nella lista!");
+    } else {
+        alert("Il controllo dice che non sei nella lista!");
+    } 
+});
+
